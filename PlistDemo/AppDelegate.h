6c663e43bb8ce07966f5a4a31e26d2f2913a3aa3
silvansky <v.silvansky@gmail.com>
2012-11-15 10:39:58 +0400

    Initial Commit

---
+++ PlistDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PlistDemo
+//
+//  Created by Valentine Gorshkov on 15.11.12.
+//  Copyright (c) 2012 silvansky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ PlistDemo/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  PlistDemo
+//
+//  Created by Valentine Gorshkov on 15.11.12.
+//  Copyright (c) 2012 silvansky. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
